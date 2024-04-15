4556c4f90a642add6451c5d4fd0726886564f7a2
joefelx <joefelix.a2003@gmail.com>
2024-04-15 19:40:23 +0530

    cors update

--- server/app.js
+++ server/app.js
@@ -13,26 +13,31 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "Content-Type",
-    "Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "Content-Type",
+//     "Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET"],
+  })
+);
 
 // Database Connection
 mongoose.connect(
